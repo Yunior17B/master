7fc9172c688cbcb004022ebc58dc3d9045efb96d
Marcin Białas <marcin_19972@interia.pl>
2020-11-30 12:46:24 +0100

    chat is git

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const articleRouter = require("./routes/articles")
 const mongoose = require('mongoose')
 const Article = require ('./models/article')
 const methodOverride = require('method-override');
-const Username = require('./models/UserModel').User;
 const socket = require("socket.io");
 const app = express()
 const bodyParser = require('body-parser');
@@ -22,7 +21,9 @@ mongoose.connect('mongodb://localhost/inz',{
      useNewUrlParser: true, 
      useCreateIndex: true })
      .then(()=> console.log('MongoDb Connected'))
-     .catch(err => console.log(err))
+     .catch(err => console.log(err)), 
+
+  
 
 app.use(expressLayouts);
 app.set('view engine', 'ejs')
@@ -84,8 +85,7 @@ const server = app.listen(port, function(){
 app.get('/chatClient', function(req, res) {
   res.render('chat/chatClient.ejs');
 });
-// const rooms = { }
-// console.log(rooms)
+
 app.get('/chat', function(req, res) {
     res.render('chat/chat.ejs');
 });
@@ -93,25 +93,62 @@ app.get('/chat', function(req, res) {
 ////// Socket Setup
 
 const io = socket(server);
-io.on("connection", function (socket) {
-  //console.log("Made socket connection");
-});
-io.sockets.on('connection', function (socket) {
+mongoose.connect('mongodb://localhost/inz', function(err, db){
+    if(err){
+        throw err;
+    }
 
- // console.log("Socket connected.");
-    
-  socket.on('message', function(msg){
-    io.emit('message', msg);
-  });
-});
+    console.log('MongoDB connected...');
 
-io.on('connection', (socket)=>{
-  console.log("a user connected via socket!")
-  socket.on('disconnect', ()=>{
-      console.log("a user disconnected!")
-  })
-  socket.on('chat message', (msg)=>{
-      console.log("Message: "+msg)
-      io.emit('chat message', msg)
-  })
-})
+    // Connect to Socket.io
+    io.on('connection', function(socket){
+        let chat = db.collection('chats');
+
+        // Create function to send status
+        sendStatus = function(s){
+            socket.emit('status', s);
+        }
+
+        // Get chats from mongo collection
+        chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
+            if(err){
+                throw err;
+            }
+
+            // Emit the messages
+            socket.emit('output', res);
+        });
+
+        // Handle input events
+        socket.on('input', function(data){
+            let name = data.name;
+            let message = data.message;
+
+            // Check for name and message
+            if(name == '' || message == ''){
+                // Send error status
+                sendStatus('Please enter a name and message');
+            } else {
+                // Insert message
+                chat.insert({name: name, message: message}, function(){
+                    io.emit('output', [data]);
+
+                    // Send status object
+                    sendStatus({
+                        message: 'Message sent',
+                        clear: true
+                    });
+                });
+            }
+        });
+
+        // Handle clear
+        socket.on('clear', function(data){
+            // Remove all chats from collection
+            chat.remove({}, function(){
+                // Emit cleared
+                socket.emit('cleared');
+            });
+        });
+    });
+});
